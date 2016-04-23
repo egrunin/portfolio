0cfa466dcaccb93fae88f48d2a75deb0d4e8b320
Jack <evgeni.grunin@mygeorgian.ca>
2016-04-23 15:10:50 -0400

    upd16

--- Scripts/work.js
+++ Scripts/work.js
@@ -3,17 +3,42 @@
 // miniport
 // April, 2016
 
-(function() {
+"use strict";
 
-    "use strict";
-    var imagecount = 1;
-    var total = 3;
+$(function() {
+    // configuration
+    var width = 350;
+    var animationSpeed = 1000;
+    var pause = 5000;
+    var currentSlide = 1;
     
-    function slide(y) {
-        var Image = document.getElementById('img');
-        imagecount = imagecount + y;
-        Image.src = "images/img" + imagecount + ".jpg";
+    // cash DOM 
+    var $slider = $('#slider');
+    var $sliderContainer = $slider.find('.slides');
+    var $slides = $sliderContainer.find('.slide');
+    
+    var interval;
+    
+    function startSlider() {
+        interval = setInterval(function() {
+            $sliderContainer.animate({'margin-left': '-=' + width}, animationSpeed, function() {
+            currentSlide++;
+            if (currentSlide === $slide.length) {
+                currentSlide = 1;
+                $sliderContainer.css('margin-left', 0); 
+            } 
+            });
+        }, pause);
     }
     
+    function stopSlider() {
+       clearInterval(interval); 
+    }
     
-})();
+    //listen to mouse enter and pause
+    //resume on mouseleave
+    $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+    
+    startSlider();
+    
+});
