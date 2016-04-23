7b6da34bce67773b77cb3557df60fe31748b3dd5
Jack <evgeni.grunin@mygeorgian.ca>
2016-04-23 16:31:18 -0400

    upd21

--- Scripts/work.js
+++ Scripts/work.js
@@ -37,3 +37,50 @@ var time = window.setInterval(function photoA() {
 	if(imageCount < 1){imageCount = total;}	
 	image.src = "Images/img"+ imageCount +".jpg";
 	},5000);
+    
+    
+// mouseover/mouseout functions for Percolator page
+    document.getElementById("percolator").addEventListener("mouseover", mouseOverperc);
+    document.getElementById("percolator").addEventListener("mouseout", mouseOutperc);
+
+    var percInfo = "My first EVER webpage!";
+
+    function mouseOverperc() {
+        document.getElementById("percolator").innerHTML = percInfo;
+        document.getElementById("percolator").style.color = "violet";
+    };
+    function mouseOutperc() {
+        document.getElementById("percolator").style.color = "red";
+    };
+
+
+
+    // mouseover/mouseout functions for Patent page
+    document.getElementById("patent").addEventListener("mouseover", mouseOverpat);
+    document.getElementById("patent").addEventListener("mouseout", mouseOutpat);
+
+    var patInfo = "My first EVER page design!";
+
+    function mouseOverpat() {
+        document.getElementById("patent").innerHTML = patInfo;
+        document.getElementById("patent").style.color = "violet";
+    };
+    function mouseOutpat() {
+        document.getElementById("patent").style.color = "red";
+    };
+
+
+
+    // mouseover/mouseout functions for CMS page
+    document.getElementById("perscms").addEventListener("mouseover", mouseOverprod);
+    document.getElementById("perscms").addEventListener("mouseout", mouseOutprod);
+
+    var prodInfo = "My first EVER server side script!";
+
+    function mouseOverprod() {
+        document.getElementById("perscms").innerHTML = prodInfo;
+        document.getElementById("perscms").style.color = "violet";
+    };
+    function mouseOutprod() {
+        document.getElementById("perscms").style.color = "red";
+    };
