59635a0df0add21ec5cf6b202c01acbe8d9cc5e6
Jack <evgeni.grunin@mygeorgian.ca>
2016-04-23 16:07:20 -0400

    upd18

--- Scripts/work.js
+++ Scripts/work.js
@@ -5,7 +5,34 @@
 
 "use strict";
 
-$(function() {
+var imageCount = 1;
+var total = 6;
+
+function photo(x) {
+	var image = document.getElementById('image');
+	imageCount = imageCount + x;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "Images/img"+ imageCount +".jpg";
+	clearInterval(time); 								// clear interval stops the set interval.
+	time =  window.setInterval(function photoA() { 		// givig the value of time the samfunction below starts the loop 
+	var image = document.getElementById('image');
+	imageCount = imageCount + 1;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "Images/img"+ imageCount +".jpg";
+	},2000);
+	}
+ 
+var time = window.setInterval(function photoA() {    // just addign the sunction to the variable so you can target it.
+	var image = document.getElementById('image');
+	imageCount = imageCount + 1;
+	if(imageCount > total){imageCount = 1;}
+	if(imageCount < 1){imageCount = total;}	
+	image.src = "Images/img"+ imageCount +".jpg";
+	},2000);
+
+/*$(function() {
     // configuration
     var width = 350;
     var animationSpeed = 1000;
@@ -41,4 +68,4 @@ $(function() {
     
     startSlider();
     
-});
+});*/
